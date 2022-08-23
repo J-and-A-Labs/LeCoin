78f3dbfd53dd5b68bba81be23a2201a400e4f364
Mr-Yeety <mryeety1@gmail.com>
2022-08-23 09:06:30 -0600

    Changed Language to python

---
+++ Block.py
@@ -0,0 +1,11 @@
+class Block:
+    #Add Transaction class Here
+     def __init__(self,prevHash):
+        self.prevHash = prevHash
+
+
+     def calculateIncompleteHash(transactionHash, prevHash):
+        pass
+
+     def calculateHash(incompleteHash, ProofOfWork):
+        pass 
