bbe51f6595106783cbf61ffd81e3336bfddd5db4
ECO3X10C <adityakoduri1@gmail.com>
2022-08-23 16:19:15 -0600

    Mining Confirmation
    indeed

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ block = Block("dn", "uhkuyibkj", pool)
 print(pool.queue)
 
 while not pool.queue[0].approved:
-    pow = random.randint(1,1000)
-    print(pow)
-    pool.queue[0].calculateHash(pow) 
-    print(pool.queue[0].hash)
+    mine = input("Would you like to start mining?")
+    if mine == "y":
+        pow = random.randint(1,1000)
+        print(pow)
+        pool.queue[0].calculateHash(pow) 
+        print(pool.queue[0].hash)
+    else:
+        print("Mining Cancelled.")
 
 print(f"Proof of work has been done for Block with Id {block.blockId} with challenge number {pow}.")
 
